591867db92914b358b1293b18c4b4eb343fea1e5
Jeff Anderson <jander99@gmail.com>
2020-04-25 22:43:07 -0400

    Use docker network instead of compose

--- scripts/network-setup.sh
+++ scripts/network-setup.sh
@@ -3,4 +3,8 @@
 # Use docker-compose to build the networks
 export IPINTERFACE=`ip link | grep "MASTER" | awk -F: '$0 !~ "lo|vir|wl|^[^0-9]"{print $2;getline}' | awk '{$1=$1};1'`
 
-docker-compose -f networks-compose.yml --verbose up 
+docker network create -d macvlan -o parent ${IPINTERFACE} \
+  --subnet 192.168.1.0/24 \
+  --gateway 192.168.1.1 \
+  --ip-range 192.168.1.0/28 \
+  homelab_physical_network
