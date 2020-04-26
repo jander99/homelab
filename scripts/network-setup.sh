6a7cbd1f42821cef0eb073582672896d3162df53
Jeff Anderson <jander99@gmail.com>
2020-04-25 22:54:53 -0400

    Remove compose for networks

--- scripts/network-setup.sh
+++ scripts/network-setup.sh
@@ -3,8 +3,5 @@
 # Use docker-compose to build the networks
 export IPINTERFACE=`ip link | grep "MASTER" | awk -F: '$0 !~ "lo|vir|wl|^[^0-9]"{print $2;getline}' | awk '{$1=$1};1'`
 
-docker network create -d macvlan -o parent ${IPINTERFACE} \
-  --subnet 192.168.1.0/24 \
-  --gateway 192.168.1.1 \
-  --ip-range 192.168.1.0/28 \
-  homelab_physical_network
+docker network create -d macvlan -o parent=${IPINTERFACE} --subnet 192.168.1.0/24 --gateway 192.168.1.1 --ip-range 192.168.1.0/28 homelab_physical_network
+
